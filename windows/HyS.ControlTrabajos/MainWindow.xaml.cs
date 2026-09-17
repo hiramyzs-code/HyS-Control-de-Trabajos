a148f66631e7e752b138a96bf529733548378a09
hiramyzs-code <hiramyzs@gmail.com>
2026-09-16 23:04:36 -0700

    Pass authenticated Supabase session into Windows workspace

--- windows/HyS.ControlTrabajos/MainWindow.xaml.cs
+++ windows/HyS.ControlTrabajos/MainWindow.xaml.cs
@@ -1,14 +1,47 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
+using HyS.ControlTrabajos.Data;
+
 namespace HyS.ControlTrabajos;
-public partial class MainWindow : Window {
- public ObservableCollection<Job> Jobs { get; } = new();
- public MainWindow(){ InitializeComponent(); JobsGrid.ItemsSource=Jobs; RefreshTotal(); }
- private void NewJob_Click(object sender,RoutedEventArgs e){
-  var dialog=new JobDialog{Owner=this};
-  if(dialog.ShowDialog()==true && dialog.Result!=null){ Jobs.Insert(0,dialog.Result); RefreshTotal(); }
- }
- private void RefreshTotal(){ TotalText.Text=Jobs.Where(j=>j.Status=="Terminado").Sum(j=>j.Amount).ToString("C2",new System.Globalization.CultureInfo("es-MX")); }
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<Job> Jobs { get; } = new();
+    public AuthSession Session { get; }
+
+    public MainWindow(AuthSession session)
+    {
+        Session = session;
+        InitializeComponent();
+        JobsGrid.ItemsSource = Jobs;
+        RefreshTotal();
+        Title = $"HyS Control de Trabajos - {Session.User.Email}";
+    }
+
+    private void NewJob_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new JobDialog { Owner = this };
+        if (dialog.ShowDialog() == true && dialog.Result != null)
+        {
+            Jobs.Insert(0, dialog.Result);
+            RefreshTotal();
+        }
+    }
+
+    private void RefreshTotal()
+    {
+        TotalText.Text = Jobs.Where(j => j.Status == "Terminado").Sum(j => j.Amount)
+            .ToString("C2", new System.Globalization.CultureInfo("es-MX"));
+    }
 }
-public class Job { public string Date {get;set;}=""; public string Area {get;set;}=""; public string Work {get;set;}=""; public decimal Amount {get;set;} public string Status {get;set;}="Pendiente"; }
+
+public class Job
+{
+    public Guid Id { get; set; }
+    public string Date { get; set; } = "";
+    public string Area { get; set; } = "";
+    public string Work { get; set; } = "";
+    public decimal Amount { get; set; }
+    public string Status { get; set; } = "Pendiente";
+}
